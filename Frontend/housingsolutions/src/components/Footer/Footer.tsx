398bcb6b69fc0da46be7270c0c3097c75c83bb0c
Nivedita Bhat <nivedita@lt275.vacansoleil.local>
2020-01-24 16:47:52 +0100

    initialize footer component

---
+++ Frontend/housingsolutions/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styles from "./Footer.module.scss";
+
+interface IFooterProps {}
+
+const Footer: React.FunctionComponent<IFooterProps> = props => {
+  return <footer>footer</footer>;
+};
+
+export default Footer;
