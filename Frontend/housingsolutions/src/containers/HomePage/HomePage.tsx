c060617be4f916e6895b588385bfb766e6f94c0e
Nivedita Bhat <nivedita@lt275.vacansoleil.local>
2020-01-24 16:48:22 +0100

    initialize homepage component which will serve the main content of the page

---
+++ Frontend/housingsolutions/src/containers/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styles from "./HomePage.module.scss";
+
+interface IHomePageProps {}
+
+const HomePage: React.FunctionComponent<IHomePageProps> = props => {
+  return <section>main</section>;
+};
+
+export default HomePage;
