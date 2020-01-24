3a5024b2a9e183ef5fcb1e4179c5b619cee7d11e
Nivedita Bhat <nivedita@lt275.vacansoleil.local>
2020-01-24 16:47:23 +0100

    add layout components - Header, Footer and Main to App/root component

--- Frontend/housingsolutions/src/containers/App/App.tsx
+++ Frontend/housingsolutions/src/containers/App/App.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import styles from "./App.module.scss";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import HomePage from "../HomePage/HomePage";
 
 interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = props => {
   return (
-    <section className={styles.main}>
-      <h1>Main</h1>
-      <p>hello</p>
-    </section>
+    <main className={styles.MainWrapper}>
+      <Header />
+      <HomePage />
+      <Footer />
+    </main>
   );
 };
 
