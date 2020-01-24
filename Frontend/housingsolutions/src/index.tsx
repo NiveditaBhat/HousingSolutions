77c25711e8f5e64e7a78206a909450e09c98d8db
Nivedita Bhat <nivedita@lt275.vacansoleil.local>
2020-01-24 14:51:17 +0100

    initialize route for App component

--- Frontend/housingsolutions/src/index.tsx
+++ Frontend/housingsolutions/src/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import App from "./containers/App/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
