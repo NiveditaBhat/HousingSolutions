8ec5aee2ac8506f096f92e6769191dc7a45884fb
Nivedita Bhat <nivedita@lt278.vacansoleil.local>
2020-01-25 12:05:23 +0100

    design UI for header component, apply styling for mobile,tablet and desktop devices

--- Frontend/housingsolutions/src/components/Header/Header.tsx
+++ Frontend/housingsolutions/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import styles from "./Header.module.scss";
 import { ReactComponent as CompanyLogo } from "../../static/svgs/companylogo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface IHeaderProps {}
 
@@ -12,9 +13,17 @@ const Header: React.FunctionComponent<IHeaderProps> = props => {
           <CompanyLogo className={styles.Header_logo} />
           <span className={styles.Header_title}>HousingSolutions </span>
         </li>
-        <li className={styles.Header_item}>Contact</li>
-        <li className={styles.Header_item}>Login</li>
-        <li className={styles.Header_item}>Register</li>
+        <li className={styles.Header_item}>
+          <section className={styles.Header_contact}>
+            <FontAwesomeIcon icon="phone-alt" className={styles.Header_phoneIcon} />
+            <span>call us - 01633 030040</span>
+          </section>
+        </li>
+        <li className={`${styles.Header_item} ${styles.Header_item___login}`}>Login</li>
+        <li className={`${styles.Header_item} ${styles.Header_item___register}`}>Register</li>
+        <li className={`${styles.Header_item} ${styles.Header_item___menuBar}`}>
+          <FontAwesomeIcon icon="bars" className={styles.Header_phoneIcon} />
+        </li>
       </ul>
     </header>
   );
