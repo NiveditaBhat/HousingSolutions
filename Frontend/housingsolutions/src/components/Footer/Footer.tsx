dce76c3a4d400020445523b9baf593e343b82adb
Nivedita Bhat <nivedita@LT345.local>
2020-01-26 16:58:20 +0100

    add footerItem and socialMedia component to footer

--- Frontend/housingsolutions/src/components/Footer/Footer.tsx
+++ Frontend/housingsolutions/src/components/Footer/Footer.tsx
@@ -1,10 +1,41 @@
 import * as React from "react";
 import styles from "./Footer.module.scss";
+import FooterItem from "../FooterItem/FooterItem";
+import SocialMediaIcons from "../SocialMediaIcons/SocialMediaIcons";
 
 interface IFooterProps {}
 
 const Footer: React.FunctionComponent<IFooterProps> = props => {
-  return <footer>footer</footer>;
+  const footerlinks = [
+    {
+      title: "Get Started",
+      subLinks: ["Home", "Sign up", "Downloads"],
+    },
+    {
+      title: "About Us",
+      subLinks: ["Company Information", "Contact Us", "Reviews"],
+    },
+    {
+      title: "Support",
+      subLinks: ["FAQ", "Help Desk", "Forums"],
+    },
+    {
+      title: "Legal",
+      subLinks: ["Terms Of Service", "Terms Of Use", "Privacy Policy"],
+    },
+  ];
+
+  return (
+    <footer className={styles.Footer}>
+      <section className={styles.Footer_mainLinks}>
+        {footerlinks.map(footerLink => (
+          <FooterItem title={footerLink.title} subLinks={footerLink.subLinks} />
+        ))}
+      </section>
+      <SocialMediaIcons />
+      <section className={styles.Footer_subFooter}>@2020 Copyright HousingSolutions</section>
+    </footer>
+  );
 };
 
 export default Footer;
