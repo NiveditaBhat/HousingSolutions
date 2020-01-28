fc1585ace700d9604fab945f61a3c7f52a9e5c7d
Nivedita Bhat <nivedita@lt278.vacansoleil.local>
2020-01-28 16:29:24 +0100

    refactor router, add switch

--- Frontend/housingsolutions/src/containers/App/App.tsx
+++ Frontend/housingsolutions/src/containers/App/App.tsx
@@ -3,15 +3,18 @@ import styles from "./App.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import HomePage from "../HomePage/HomePage";
+import { Route, Switch } from "react-router-dom";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = props => {
   return (
     <main className={styles.MainWrapper}>
-      <Header />
-      <HomePage />
-      <Footer />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
     </main>
   );
 };
--- Frontend/housingsolutions/src/index.tsx
+++ Frontend/housingsolutions/src/index.tsx
@@ -1,28 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import App from "./containers/App/App";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faPhoneAlt, faBars, faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneAlt, faBars, faWindowClose, faBackward } from "@fortawesome/free-solid-svg-icons";
 
-const routing = (
+library.add(fab, faPhoneAlt, faBars, faWindowClose, faBackward);
+ReactDOM.render(
   <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      {/*   <Route  path="/search" component={SearchProperties} />
-      <Route  path="/property" component={PropertyDetails} />
-      <Route  path="/book" component={BookProperty} />
-      <Route  path="/login" component={UserLogin} />
-      <Route  path="/singup" component={UserSignup} /> */}
-    </div>
-  </Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
-
-library.add(fab, faPhoneAlt, faBars, faWindowClose);
-ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
