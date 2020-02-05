0ef30f120a6565b6608c81b332f495b157c7c61e
Nivedita Bhat <nivedita@lt278.vacansoleil.local>
2020-02-05 19:08:23 +0100

    design UI of search container- add dropdowns

--- Frontend/housingsolutions/src/components/SearchContainer/SearchContainer.tsx
+++ Frontend/housingsolutions/src/components/SearchContainer/SearchContainer.tsx
@@ -1,33 +1,47 @@
 import * as React from "react";
 import styles from "./SearchContainer.module.scss";
 import Dropdown from "../Dropdown/Dropdown";
+import Button from "../Button/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useMedia from "../../utils/useMedia";
 
 const SearchContainer: React.FunctionComponent = () => {
+  const isTablet = useMedia("(min-width:48em)");
+
   return (
     <form>
       <section className={styles.SearchContainer}>
+        <div className={styles.SearchContainer_titleBlock}>
+          <span className={styles.SearchContainer_title}>Find your Rental Property now</span>
+        </div>
         <div className={styles.SearchContainer_cityBlock}>
+          <Dropdown label="Country" options={["Furnished", "Unfurnished"]} />
+        </div>
+        <div className={styles.SearchContainer_buttonBlock}>
+          <Button
+            type="primary"
+            label={"Search"}
+            onClick={() => alert("ff")}
+            extraClasses={[styles.SearchContainer_searchButton]}
+          >
+            {isTablet && <FontAwesomeIcon icon="search" />}
+          </Button>
+        </div>
+        <div className={styles.SearchContainer_priceBlock}>
           <Dropdown
             label="Price"
-            options={[
-              { key: "one", value: "Furnished" },
-              { key: "two", value: "Unfurnished" },
-            ]}
+            options={["> €500", "> €1000", "> €2000", "> €3000", "> €4000", "> €5000", "> €7000"]}
           />
         </div>
-        <div className={styles.SearchContainer_countryBlock}>
-          <Dropdown
-            label="country"
-            options={[
-              { key: "three", value: "Furnished" },
-              { key: "four", value: "Unfurnished" },
-            ]}
-          />
+        <div className={styles.SearchContainer_bedroomsBlock}>
+          <Dropdown label="Bedrooms" options={["1", "2", "3", "4", "5"]} />
         </div>
-        <div className={styles.SearchContainer_titleBlock}>Search</div>
-        <div className={styles.SearchContainer_priceBlock}>Price</div>
-        <div className={styles.SearchContainer_bedroomsBlock}>Bedroom</div>
-        <div className={styles.SearchContainer_categoryBlock}>Category</div>
+        <div className={styles.SearchContainer_interiorBlock}>
+          <Dropdown label="Interior" options={["Furnished", "Unfurnished", "Semi-furnished"]} />
+        </div>
+        <div className={styles.SearchContainer_categoryBlock}>
+          <Dropdown label="Category" options={["Apartment", "Room", "Bungalow", "House"]} />
+        </div>
       </section>
     </form>
   );
