59c247b0765eae280ec13938afba459e7a0eb4db
Nivedita Bhat <nivedita@LT345.local>
2020-02-09 14:00:55 +0100

    add new icon 'times'

--- Frontend/housingsolutions/src/index.tsx
+++ Frontend/housingsolutions/src/index.tsx
@@ -14,6 +14,7 @@ import {
   faChevronDown,
   faChevronUp,
   faSearch,
+  faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
@@ -24,7 +25,8 @@ library.add(
   faBackward,
   faChevronDown,
   faChevronUp,
-  faSearch
+  faSearch,
+  faTimes
 );
 ReactDOM.render(
   <Router>
