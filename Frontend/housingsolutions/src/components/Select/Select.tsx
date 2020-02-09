b455d02af475afc0990fe427025d32b832eb9e6b
Nivedita Bhat <nivedita@LT345.local>
2020-02-09 14:00:19 +0100

    refactor Select component, show dropdown component for desktop devices and popup component for mobile/tablet devices

--- Frontend/housingsolutions/src/components/Select/Select.tsx
+++ Frontend/housingsolutions/src/components/Select/Select.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import styles from "./Select.module.scss";
-import Button from "../Button/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ClickOutside from "../ClickOutside/ClickOutside";
 import Dropdown from "../Dropdown/Dropdown";
 import SelectButton from "../SelectButton/SelectButton";
+import useMedia from "../../utils/useMedia";
+import Popup from "../Popup/Popup";
 
 interface SelectProps {
   label: string;
@@ -13,11 +13,13 @@ interface SelectProps {
 
 const Select: React.FunctionComponent<SelectProps> = ({ label, options }) => {
   const [toggle, setToggle] = React.useState(false);
+  const isDesktop = useMedia("(min-width:64em)");
 
   const handleToggle = React.useCallback(
     e => {
       e.preventDefault();
       setToggle(!toggle);
+      console.log(toggle);
     },
     [toggle]
   );
@@ -31,13 +33,19 @@ const Select: React.FunctionComponent<SelectProps> = ({ label, options }) => {
     [toggle]
   );
 
-  return (
-    <ClickOutside onClickOutside={handleClickOutside}>
-      <section className={styles.Select}>
-        <SelectButton label={label} handleToggle={handleToggle} toggle={toggle} />
-        {toggle && <Dropdown label={label} options={options} />}
-      </section>
-    </ClickOutside>
+  const Select = (
+    <section className={styles.Select}>
+      <SelectButton label={label} handleToggle={handleToggle} toggle={toggle} />
+      {isDesktop
+        ? toggle && <Dropdown label={label} options={options} />
+        : toggle && <Popup label={label} options={options} handleToggle={handleClickOutside} />}
+    </section>
+  );
+
+  return isDesktop ? (
+    <ClickOutside onClickOutside={handleClickOutside}>{Select}</ClickOutside>
+  ) : (
+    Select
   );
 };
 
