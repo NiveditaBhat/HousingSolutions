4d9d488ed78057a8814eb16aedd4caa46794be96
Nivedita Bhat <nivedita@LT345.local>
2020-02-21 11:42:34 +0100

    add Icon component to add icon with text

---
+++ Frontend/housingsolutions/src/components/Icon/Icon.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import styles from "./Icon.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
+
+interface IconProps {
+  text: string;
+  name:
+    | "phone-alt"
+    | "bars"
+    | "window-close"
+    | "backward"
+    | "chevron-down"
+    | "chevron-up"
+    | "search"
+    | "times"
+    | "bed"
+    | "bath"
+    | "map-marker-alt";
+  extraClasses?: string[];
+}
+
+const Icon: React.FunctionComponent<IconProps> = ({ text, name, extraClasses }: IconProps) => {
+  return (
+    <section className={classNames(styles.Icon, extraClasses)}>
+      <FontAwesomeIcon icon={name} />
+      <span>{text}</span>
+    </section>
+  );
+};
+
+export default Icon;
