edc583eabf5dfa943daa8896309a7684e2d39d0e
Nivedita Bhat <nivedita@LT345.local>
2020-02-22 20:45:21 +0100

    add about us component to home page

---
+++ Frontend/housingsolutions/src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import styles from "./AboutUs.module.scss";
+import realEstateImage from "../../static/images/real-estate.jpg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
+import aboutUs from "../../utils/AboutUs.settings.json";
+
+interface AboutUsProps {}
+
+const AboutUs: React.FunctionComponent<AboutUsProps> = () => {
+  return (
+    <section className={styles.AboutUs}>
+      <h2 className={styles.AboutUs_title}>
+        <span>What is Housing Solutions</span>
+      </h2>
+      <div className={styles.AboutUs_section}>
+        <div className={styles.AboutUs_info}>
+          <table className={styles.AboutUs_infoList}>
+            <tbody>
+              {aboutUs.map(item => (
+                <tr key={item.icon}>
+                  <td>
+                    <FontAwesomeIcon icon={item.icon as IconName} />
+                  </td>
+                  <td>{item.text}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <img className={styles.AboutUs_img} src={realEstateImage} alt="realEstate" />
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
