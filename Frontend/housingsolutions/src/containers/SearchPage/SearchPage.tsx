243440e14490c0f9b7976bc14b7404405180a644
Nivedita Bhat <nivedita@LT345.local>
2020-03-06 16:41:18 +0100

    update search-page, connect it to store, fetch searchInput and make a graphql query to filter properties

--- Frontend/housingsolutions/src/containers/SearchPage/SearchPage.tsx
+++ Frontend/housingsolutions/src/containers/SearchPage/SearchPage.tsx
@@ -3,23 +3,48 @@ import styles from "./SearchPage.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import SearchResults from "../../components/SearchResults/SearchResults";
-import SearchContainer from "../../components/SearchContainer/SearchContainer";
-import { GET_ALL_PROPERTIES } from "../../gql/getAllProperties";
 import { useQuery } from "@apollo/react-hooks";
+import * as types from "../../types";
+import { FILTER_PROPERTIES } from "../../gql/filterProperties";
+import Loader from "../../components/Loader/Loader";
+import SearchError from "../../components/SearchError/SearchError";
 import Search from "../../components/Search/Search";
+import { connect } from "react-redux";
 
-const SearchPage: React.FunctionComponent = () => {
-  //const { loading, data, error } = useQuery(GET_ALL_PROPERTIES);
+interface SearchPageProps {
+  searchFilter: types.SearchInput;
+}
 
+interface RootState {
+  search: { searchFilter: types.SearchInput };
+}
+
+const mapStateToProps = (state: RootState) => ({
+  searchFilter: state.search.searchFilter,
+});
+
+const SearchPage: React.FunctionComponent<SearchPageProps> = ({ searchFilter }) => {
+  console.log(searchFilter);
+  const { loading, data, error } = useQuery(FILTER_PROPERTIES, {
+    variables: { searchFilter },
+  });
+  console.log(data);
   return (
-    <section className={styles.Search}>
+    <section className={styles.SearchPage}>
       <Header />
-      <section className={styles.Search_container}>
-        <Search />
+      <section className={styles.SearchPage_container}>
+        <div className={styles.SearchPage_heroImage} />
+        <Search extraClasses={[styles.SearchPage_search]} />
+        {loading ? (
+          <Loader />
+        ) : data && !error ? (
+          <SearchResults properties={data.property.searchProperties} />
+        ) : (
+          <SearchError />
+        )}
       </section>
       <Footer />
     </section>
   );
 };
-
-export default SearchPage;
+export default connect(mapStateToProps)(SearchPage);
