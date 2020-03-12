3605851afdf391e7f656ac6fc3d9f2d0348af0f5
Nivedita Bhat <nivedita@lt270.vacansoleil.local>
2020-03-12 17:58:36 +0100

    update SearchResults to add extraClasses prop to modify styling

--- Frontend/housingsolutions/src/components/SearchResults/SearchResults.tsx
+++ Frontend/housingsolutions/src/components/SearchResults/SearchResults.tsx
@@ -3,23 +3,32 @@ import * as types from "../../types";
 import styles from "./SearchResults.module.scss";
 import PropertyList from "../PropertyList/PropertyList";
 import Button from "../Button/Button";
+import classNames from "classnames";
+import SortProperties from "../SortProperties/SortProperties";
 
 interface SearchResultsProps {
   properties: types.PropertyType[];
+  extraClasses?: string[];
 }
 
-const SearchResults: React.FunctionComponent<SearchResultsProps> = ({ properties }) => {
+const SearchResults: React.FunctionComponent<SearchResultsProps> = ({
+  properties,
+  extraClasses,
+}) => {
   return (
-    <section className={styles.SearchResults}>
+    <section className={classNames(styles.SearchResults, extraClasses)}>
       {properties.length > 0 ? (
         <>
           <header className={styles.SearchResults_header}>
-            <h2 className={styles.SearchResults_title}>
-              <span>Rental Apartments in The Netherlands</span>
-            </h2>
-            <span>
-              <strong>{properties.length} properties</strong>
-            </span>
+            <div className={styles.SearchResults_titleContainer}>
+              <h2 className={styles.SearchResults_title}>
+                <span>Rental Apartments in The Netherlands</span>
+              </h2>
+              <span>
+                <strong>{properties.length} properties</strong>
+              </span>
+            </div>
+            <SortProperties />
           </header>
           <PropertyList properties={properties} />
           <Button
