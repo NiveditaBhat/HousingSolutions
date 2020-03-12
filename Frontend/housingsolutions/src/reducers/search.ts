08c14dd0d06dc3a1682381c134baa083bb075462
Nivedita Bhat <nivedita@lt270.vacansoleil.local>
2020-03-12 17:48:51 +0100

    update reducer for sort action

--- Frontend/housingsolutions/src/reducers/search.ts
+++ Frontend/housingsolutions/src/reducers/search.ts
@@ -1,12 +1,14 @@
-import { UPDATE_FILTER } from "../constants/ActionConstants";
+import { UPDATE_SEARCH_FILTER, UPDATE_SORT_FILTER } from "../constants/ActionConstants";
 
-const initialState = { searchFilter: {} };
+const initialState = { searchFilter: {}, sortFilter: {} };
 
 export default function search(state = initialState, action: any) {
   switch (action.type) {
-    case UPDATE_FILTER:
-      const filter = action.payload;
-      return { ...state, searchFilter: filter };
+    case UPDATE_SEARCH_FILTER:
+      return { ...state, searchFilter: action.payload };
+
+    case UPDATE_SORT_FILTER:
+      return { ...state, sortFilter: action.payload };
 
     default:
       return state;
