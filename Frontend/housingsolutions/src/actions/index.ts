397a8997edc9ac84af9c847c3d12d59e3c40eb99
Nivedita Bhat <nivedita@lt270.vacansoleil.local>
2020-03-12 17:47:18 +0100

    add action to update sort filter

--- Frontend/housingsolutions/src/actions/index.ts
+++ Frontend/housingsolutions/src/actions/index.ts
@@ -1,7 +1,12 @@
 import * as types from "../../src/types";
 import * as constants from "../constants/ActionConstants";
 
-export const updateFilter = (filter: types.SearchInput): types.UpdateFilterAction => ({
-  type: constants.UPDATE_FILTER,
+export const updateSearchFilter = (filter: types.SearchInput): types.UpdateSearchFilterAction => ({
+  type: constants.UPDATE_SEARCH_FILTER,
+  payload: filter,
+});
+
+export const updateSortFilter = (filter: types.SortByFields): types.UpdateSortFilterAction => ({
+  type: constants.UPDATE_SORT_FILTER,
   payload: filter,
 });
