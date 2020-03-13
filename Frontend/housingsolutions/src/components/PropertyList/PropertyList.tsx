e203745774fe4fa735942e4eb6a873ce67fb6ce8
Nivedita Bhat <nivedita@LT345.local>
2020-03-13 14:21:21 +0100

    update propertyList component - navigate to property detail page on click of property

--- Frontend/housingsolutions/src/components/PropertyList/PropertyList.tsx
+++ Frontend/housingsolutions/src/components/PropertyList/PropertyList.tsx
@@ -2,18 +2,26 @@ import * as React from "react";
 import styles from "./PropertyList.module.scss";
 import * as types from "../../types";
 import Property from "../Property/Property";
+import { useHistory } from "react-router-dom";
 
 interface PropertyListProps {
   properties: types.PropertyType[];
 }
 
 const PropertyList: React.FunctionComponent<PropertyListProps> = ({ properties }) => {
+  const history = useHistory();
   return (
     <section className={styles.PropertyList}>
       {properties.map(property => (
-        <a href="#" key={property.id} className={styles.PropertyList_anchor}>
+        <div
+          key={property.id}
+          className={styles.PropertyList_anchor}
+          onClick={() => {
+            history.push(`/property/${property.id}`);
+          }}
+        >
           <Property property={property} />
-        </a>
+        </div>
       ))}
     </section>
   );
