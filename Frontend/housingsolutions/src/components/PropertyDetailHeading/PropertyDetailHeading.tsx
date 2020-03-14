082d630d94038c12cb4148616988bd76703501fe
Nivedita Bhat <nivedita@LT345.local>
2020-03-14 18:03:19 +0100

    add heading for property detail page with anchors to search page

---
+++ Frontend/housingsolutions/src/components/PropertyDetailHeading/PropertyDetailHeading.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import styles from "./PropertyDetailHeading.module.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import * as types from "../../types";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { capitalize } from "../../utils/Capitalize";
+
+interface PropertyListProps {
+  propertyDetail: types.PropertyType;
+}
+
+const PropertyDetailHeading: React.FunctionComponent<PropertyListProps> = ({ propertyDetail }) => {
+  return (
+    <header className={styles.PropertyDetailHeading}>
+      <h1 className={styles.PropertyDetailHeading_title}>
+        <span>{propertyDetail.name}</span>
+      </h1>
+      <div className={styles.PropertyDetailHeading_subTitle}>
+        <span className={styles.PropertyDetailHeading_navigation}>
+          <Link to="/">
+            <FontAwesomeIcon icon="home" />
+          </Link>
+        </span>
+        <span className={styles.PropertyDetailHeading_navigation}>></span>
+        <span className={styles.PropertyDetailHeading_navigation}>
+          <Link to={`/search?city=${propertyDetail.address?.city}`}>
+            {propertyDetail.address?.city}
+          </Link>
+        </span>
+        <span className={styles.PropertyDetailHeading_navigation}>></span>
+        <span className={styles.PropertyDetailHeading_navigation}>
+          <Link to={`/search?category=${capitalize(propertyDetail.category)}`}>
+            {capitalize(propertyDetail.category)}
+          </Link>
+        </span>
+        <span className={styles.PropertyDetailHeading_navigation}>></span>
+        <span className={styles.PropertyDetailHeading_navigation}>{propertyDetail.name}</span>
+      </div>
+    </header>
+  );
+};
+
+export default PropertyDetailHeading;
