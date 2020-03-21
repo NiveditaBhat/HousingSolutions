5505a74dee9dc2116e7d5a5b057aecf716fd7fe9
Nivedita Bhat <nivedita@LT345.local>
2020-03-21 16:54:20 +0100

    add new component for property detail heading

--- Frontend/housingsolutions/src/components/PropertyDetailHeading/PropertyDetailHeading.tsx
+++ Frontend/housingsolutions/src/components/PropertyDetailHeading/PropertyDetailHeading.tsx
@@ -12,29 +12,31 @@ interface PropertyListProps {
 const PropertyDetailHeading: React.FunctionComponent<PropertyListProps> = ({ propertyDetail }) => {
   return (
     <header className={styles.PropertyDetailHeading}>
-      <h1 className={styles.PropertyDetailHeading_title}>
-        <span>{propertyDetail.name}</span>
-      </h1>
-      <div className={styles.PropertyDetailHeading_subTitle}>
-        <span className={styles.PropertyDetailHeading_navigation}>
-          <Link to="/">
-            <FontAwesomeIcon icon="home" />
-          </Link>
-        </span>
-        <span className={styles.PropertyDetailHeading_navigation}>></span>
-        <span className={styles.PropertyDetailHeading_navigation}>
-          <Link to={`/search?city=${propertyDetail.address?.city}`}>
-            {propertyDetail.address?.city}
-          </Link>
-        </span>
-        <span className={styles.PropertyDetailHeading_navigation}>></span>
-        <span className={styles.PropertyDetailHeading_navigation}>
-          <Link to={`/search?category=${capitalize(propertyDetail.category)}`}>
-            {capitalize(propertyDetail.category)}
-          </Link>
-        </span>
-        <span className={styles.PropertyDetailHeading_navigation}>></span>
-        <span className={styles.PropertyDetailHeading_navigation}>{propertyDetail.name}</span>
+      <div className={styles.PropertyDetailHeading_titleWrapper}>
+        <h1 className={styles.PropertyDetailHeading_title}>
+          <span>{propertyDetail.name}</span>
+        </h1>
+        <div className={styles.PropertyDetailHeading_subTitle}>
+          <span className={styles.PropertyDetailHeading_navigation}>
+            <Link to="/">
+              <FontAwesomeIcon icon="home" />
+            </Link>
+          </span>
+          <span className={styles.PropertyDetailHeading_navigation}>></span>
+          <span className={styles.PropertyDetailHeading_navigation}>
+            <Link to={`/search?city=${propertyDetail.address?.city}`}>
+              {propertyDetail.address?.city}
+            </Link>
+          </span>
+          <span className={styles.PropertyDetailHeading_navigation}>></span>
+          <span className={styles.PropertyDetailHeading_navigation}>
+            <Link to={`/search?category=${capitalize(propertyDetail.category)}`}>
+              {capitalize(propertyDetail.category)}
+            </Link>
+          </span>
+          <span className={styles.PropertyDetailHeading_navigation}>></span>
+          <span className={styles.PropertyDetailHeading_navigation}>{propertyDetail.name}</span>
+        </div>
       </div>
     </header>
   );
