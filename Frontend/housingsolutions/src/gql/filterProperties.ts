e95eb3c26ac8b47dd8da63d14174e21a4eb4140d
Nivedita Bhat <nivedita@LT345.local>
2020-04-05 16:54:31 +0200

    update filter properties query

--- Frontend/housingsolutions/src/gql/filterProperties.ts
+++ Frontend/housingsolutions/src/gql/filterProperties.ts
@@ -1,9 +1,21 @@
 import { gql } from "apollo-boost";
 
 export const FILTER_PROPERTIES = gql`
-  query searchProperties($searchFilter: SearchInput, $sortParams: SortByFields, $order: SortOrder) {
+  query searchProperties(
+    $searchFilter: SearchInput
+    $sortParams: SortByFields
+    $order: SortOrder
+    $offset: Int
+    $limit: Int
+  ) {
     property {
-      searchProperties(filterParams: $searchFilter, sortParams: $sortParams, order: $order) {
+      searchProperties(
+        filterParams: $searchFilter
+        sortParams: $sortParams
+        order: $order
+        offset: $offset
+        limit: $limit
+      ) {
         id
         name
         bedroom
