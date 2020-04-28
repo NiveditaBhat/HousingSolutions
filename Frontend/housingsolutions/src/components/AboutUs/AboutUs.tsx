3263cb55c30e479bab928c69cf6c5d39578bee56
Nivedita Bhat <nivedita@LT345.local>
2020-04-28 09:03:22 +0200

    refactor About us component

--- Frontend/housingsolutions/src/components/AboutUs/AboutUs.tsx
+++ Frontend/housingsolutions/src/components/AboutUs/AboutUs.tsx
@@ -6,29 +6,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconName } from "@fortawesome/fontawesome-svg-core";
 import { aboutUs } from "../../utils/data";
 
-interface AboutUsProps {}
-
-const AboutUs: React.FunctionComponent<AboutUsProps> = () => {
+const AboutUs: React.FunctionComponent = () => {
   return (
     <section className={styles.AboutUs}>
       <h2 className={styles.AboutUs_title}>
         <span>What is Housing Solutions</span>
       </h2>
       <div className={styles.AboutUs_section}>
-        <div className={styles.AboutUs_info}>
-          <table className={styles.AboutUs_infoList}>
-            <tbody>
-              {aboutUs.map(item => (
-                <tr key={item.icon}>
-                  <td>
-                    <FontAwesomeIcon icon={item.icon as IconName} />
-                  </td>
-                  <td>{item.text}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <table className={styles.AboutUs_infoList}>
+          <tbody>
+            {aboutUs.map(item => (
+              <tr key={item.icon}>
+                <td>
+                  <FontAwesomeIcon icon={item.icon as IconName} />
+                </td>
+                <td>{item.text}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <img className={styles.AboutUs_img} src={realEstateImage} alt="realEstate" />
       </div>
     </section>
