e437cebbc372b62aa58bafb76243091bfea78e6f
Nivedita Bhat <nivedita@LT345.local>
2020-04-28 09:02:51 +0200

    update index.tsx, add scrolltop to router

--- Frontend/housingsolutions/src/index.tsx
+++ Frontend/housingsolutions/src/index.tsx
@@ -4,80 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import App from "./containers/App/App";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
+import "./utils/faIcons";
 import { client } from "../src/utils/useClient";
 import { ApolloProvider } from "@apollo/react-hooks";
 import store from "./utils/configureStore";
 import { Provider } from "react-redux";
-
-import {
-  faPhoneAlt,
-  faBars,
-  faWindowClose,
-  faBackward,
-  faChevronDown,
-  faChevronUp,
-  faSearch,
-  faTimes,
-  faBed,
-  faBath,
-  faMapMarkerAlt,
-  faChair,
-  faHome,
-  faCheckSquare,
-  faPlus,
-  faPiggyBank,
-  faCalendarPlus,
-  faHandshake,
-  faUserClock,
-  faUsers,
-  faUserCircle,
-  faCity,
-  faEnvelope,
-  faAddressBook,
-  faCalendarWeek,
-  faMoneyBill,
-  faImage,
-  faMapMarkedAlt,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(
-  fab,
-  faPhoneAlt,
-  faBars,
-  faWindowClose,
-  faBackward,
-  faChevronDown,
-  faChevronUp,
-  faSearch,
-  faTimes,
-  faBed,
-  faBath,
-  faMapMarkerAlt,
-  faChair,
-  faPlus,
-  faCheckSquare,
-  faHome,
-  faPiggyBank,
-  faCalendarPlus,
-  faHandshake,
-  faUserClock,
-  faUsers,
-  faCity,
-  faEnvelope,
-  faAddressBook,
-  faUserCircle,
-  faCalendarWeek,
-  faMoneyBill,
-  faImage,
-  faMapMarkedAlt,
-  faChevronRight
-);
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 ReactDOM.render(
   <Router>
+    <ScrollToTop />
     <ApolloProvider client={client}>
       <Provider store={store}>
         <App />
