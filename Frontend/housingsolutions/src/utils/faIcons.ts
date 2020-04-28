f03d528a3776f2a77ec71afec274b832d943d1c2
Nivedita Bhat <nivedita@LT345.local>
2020-04-28 09:02:23 +0200

    add font awesome icons to utils

---
+++ Frontend/housingsolutions/src/utils/faIcons.ts
@@ -0,0 +1,36 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import * as fa from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  fab,
+  fa.faPhoneAlt,
+  fa.faBars,
+  fa.faWindowClose,
+  fa.faBackward,
+  fa.faChevronDown,
+  fa.faChevronUp,
+  fa.faSearch,
+  fa.faTimes,
+  fa.faBed,
+  fa.faBath,
+  fa.faMapMarkerAlt,
+  fa.faChair,
+  fa.faPlus,
+  fa.faCheckSquare,
+  fa.faHome,
+  fa.faPiggyBank,
+  fa.faCalendarPlus,
+  fa.faHandshake,
+  fa.faUserClock,
+  fa.faUsers,
+  fa.faCity,
+  fa.faEnvelope,
+  fa.faAddressBook,
+  fa.faUserCircle,
+  fa.faCalendarWeek,
+  fa.faMoneyBill,
+  fa.faImage,
+  fa.faMapMarkedAlt,
+  fa.faChevronRight
+);
