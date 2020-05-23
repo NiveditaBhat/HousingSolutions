2f6efa29d69ec01eea9a3d13ee3952ac73c0c75b
Nivedita Bhat <nbhat@vacansoleil.com>
2020-05-23 12:12:11 +0200

    update MenuBar - map through menu items

--- Frontend/housingsolutions/src/components/MenuBar/MenuBar.tsx
+++ Frontend/housingsolutions/src/components/MenuBar/MenuBar.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
 import styles from "./MenuBar.module.scss";
+import { HashLink as Link } from "react-router-hash-link";
+import { menu } from "../../utils/data";
 
-const MenuBar: React.FunctionComponent = () => {
+interface MenuBarProps {
+  onMenuClick: () => void;
+}
+
+const MenuBar: React.FunctionComponent<MenuBarProps> = ({ onMenuClick }) => {
   return (
     <section className={styles.MenuBar}>
       <ul className={styles.MenuBar_items}>
-        <li className={styles.MenuBar_item}>Login</li>
-        <li className={styles.MenuBar_item}>Register</li>
-        <li className={styles.MenuBar_item}>Home</li>
+        {menu.map(menuItem => (
+          <li key={menuItem.name} className={styles.MenuBar_item} onClick={onMenuClick}>
+            <Link to={menuItem.anchorTo}>{menuItem.name}</Link>
+          </li>
+        ))}
       </ul>
     </section>
   );
