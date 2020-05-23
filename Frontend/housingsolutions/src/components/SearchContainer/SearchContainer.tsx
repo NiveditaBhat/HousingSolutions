dbf64d498ba5baa242b246cd02b7195f05b9de63
Nivedita Bhat <nbhat@vacansoleil.com>
2020-05-23 11:11:34 +0200

    update SearchContainer - add onSubmit prop

--- Frontend/housingsolutions/src/components/SearchContainer/SearchContainer.tsx
+++ Frontend/housingsolutions/src/components/SearchContainer/SearchContainer.tsx
@@ -2,15 +2,20 @@ import * as React from "react";
 import styles from "./SearchContainer.module.scss";
 import houses from "../../static/images/houses3.jpg";
 import Search from "../Search/Search";
+import * as types from "../../types";
 
-const SearchContainer: React.FunctionComponent = () => {
+interface SearchContainerProps {
+  onSubmit?: (searchInput: types.SearchInput) => void;
+}
+
+const SearchContainer: React.FunctionComponent<SearchContainerProps> = ({ onSubmit }) => {
   return (
     <section className={styles.SearchContainer}>
       <div className={styles.SearchContainer_wrapper}>
         <h1 className={styles.SearchContainer_title}>
           <span>Easiest way to rent a home</span>
         </h1>
-        <Search navigate={true} />
+        <Search onSubmit={onSubmit} />
       </div>
       <img className={styles.SearchContainer_backgroundImage} src={houses} alt="houses" />
     </section>
