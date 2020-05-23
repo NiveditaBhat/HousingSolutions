11d41e6dfac018a40fb47e569922975e0d9b0a98
Nivedita Bhat <nbhat@vacansoleil.com>
2020-05-23 11:16:26 +0200

    modify Search component - handle onSubmit in the parent , add preselect to the Select and SearchBar based on the query param

--- Frontend/housingsolutions/src/components/Search/Search.tsx
+++ Frontend/housingsolutions/src/components/Search/Search.tsx
@@ -10,15 +10,14 @@ import * as types from "../../types";
 import { connect } from "react-redux";
 import { updateSearchFilter } from "../../actions/index";
 import { Dispatch } from "redux";
-import { useHistory } from "react-router-dom";
 import classNames from "classnames";
-import { useLocation } from "react-router";
 import queryString from "query-string";
 
 interface SearchProps {
   updateSearchFilter: (filter: types.SearchInput) => void;
   extraClasses?: string[];
-  navigate?: boolean;
+  onSubmit?: (searchInput: types.SearchInput) => void;
+  param?: queryString.ParsedQuery<string>;
 }
 
 //update 'SearchFilter' in the store whenever the search inputs change
@@ -29,30 +28,27 @@ const mapDispatchToProps = (dispatch: Dispatch<types.UpdateSearchFilterAction>) 
 const Search: React.FunctionComponent<SearchProps> = ({
   updateSearchFilter,
   extraClasses,
-  navigate,
+  onSubmit,
+  param,
 }) => {
-  const param = useLocation().search;
   const isTablet = useMedia("(min-width:48em)");
-  const [rent, setRent] = useState(0);
-  const [city, setCity] = useState("");
-  const [category, setCategory] = useState("");
-  const [interior, setInterior] = useState("");
-  const [bedroom, setBedroom] = useState(0);
-  const history = useHistory();
+  const [formState, setFormState] = useState({
+    rent: 0,
+    city: "",
+    category: "",
+    interior: "",
+    bedroom: 0,
+  });
 
-  const getFilter = React.useCallback(param => {
-    return queryString.parse(param);
-  }, []);
+  const handleStateChange = (name: string, value: string | number): void => {
+    const state = { ...formState };
+    setFormState(Object.assign(state, { [name]: value }));
+  };
 
-  React.useEffect(() => {
-    if (param) {
-      updateSearchFilter(getFilter(param));
-    }
-  }, []);
-
-  const handleSubmit = React.useCallback(
+  const submitSearchQuery = React.useCallback(
     (event: React.MouseEvent<HTMLButtonElement>) => {
       event.preventDefault();
+      const { city, rent, category, interior, bedroom } = formState;
       const searchInput = Object.assign(
         {},
         city && { city },
@@ -63,10 +59,9 @@ const Search: React.FunctionComponent<SearchProps> = ({
         { country: "NL" }
       );
       updateSearchFilter(searchInput);
-      //navigate to search page after updating the 'SearchFilter' in the store
-      if (navigate) history.push("/search");
+      onSubmit && onSubmit(searchInput);
     },
-    [city, rent, category, interior, bedroom]
+    [formState]
   );
 
   return (
@@ -76,14 +71,18 @@ const Search: React.FunctionComponent<SearchProps> = ({
           <span className={styles.Search_title}>Find your Rental Property now</span>
         </div>
         <div className={styles.Search_cityBlock}>
-          <SearchBar suggestions={searchOptions.city} onChange={value => setCity(value)} />
+          <SearchBar
+            suggestions={searchOptions.city}
+            onChange={value => handleStateChange("city", value)}
+            defaultValue={param?.city as string}
+          />
         </div>
         <div className={styles.Search_buttonBlock}>
           <Button
             category="primary"
             type="button"
             label={"Search"}
-            onClick={handleSubmit}
+            onClick={submitSearchQuery}
             extraClasses={[styles.Search_searchButton]}
           >
             {isTablet && <FontAwesomeIcon icon="search" />}
@@ -93,29 +92,36 @@ const Search: React.FunctionComponent<SearchProps> = ({
           <Select
             defaultLabel="Price"
             options={searchOptions.price}
-            onChange={value => setRent(Number(value.replace(/^\D+/g, "")))}
+            onChange={value => handleStateChange("rent", Number(value.replace(/^\D+/g, "")))}
+            preSelect={param?.rent ? ((">€" + param.rent) as string) : ""}
           />
         </div>
         <div className={styles.Search_bedroomsBlock}>
           <Select
             defaultLabel="Bedrooms"
             options={searchOptions.bedrooms}
-            onChange={value => setBedroom(+value)}
+            onChange={value => handleStateChange("bedroom", +value)}
+            preSelect={param?.bedroom as string}
           />
         </div>
         <div className={styles.Search_interiorBlock}>
           <Select
             defaultLabel="Interior"
             options={searchOptions.interior}
-            onChange={value => setInterior(value !== "No Preference" ? value : "")}
+            onChange={value =>
+              handleStateChange("interior", value !== "No Preference" ? value : "")
+            }
+            preSelect={param?.interior as string}
           />
         </div>
         <div className={styles.Search_categoryBlock}>
           <Select
             defaultLabel="Category"
             options={searchOptions.category}
-            onChange={value => setCategory(value !== "No Preference" ? value : "")}
-            preSelect={param && (getFilter(param).category as string)}
+            onChange={value =>
+              handleStateChange("category", value !== "No Preference" ? value : "")
+            }
+            preSelect={param?.category as string}
           />
         </div>
       </section>
